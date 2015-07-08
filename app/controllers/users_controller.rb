9788391d0f9b23c19b7f1327ce20cb2f360052a2
mplis <michael.plis@huffingtonpost.com>
2015-07-08 14:23:14 -0400

    add before filters to check for correct user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  
   def show
     # can tell it's :id and not :user_id from looking at output of `rake routes` -> "/users/:id"
     @user = User.find(params[:id])
